341617f9de115e1d28b67089bc7c027fac67d16e
Cao Trong Tin <tin.caotrong@gmail.com>
2020-05-13 23:53:59 +0700

    init final project UI

---
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.musicapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager(), this);
+        adapter.addFragment(new FavoriteFragment(), "Favorites");
+        adapter.addFragment(new TrackFragment(), "Tracks");
+        adapter.addFragment(new AlbumFragment(), "Albums");
+        adapter.addFragment(new TrackFragment(), "Artists");
+        adapter.addFragment(new AlbumFragment(), "Playlists");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        highLightCurrentTab(0);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+                highLightCurrentTab(position);
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+    private void highLightCurrentTab(int position) {
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            assert tab != null;
+            tab.setCustomView(null);
+            tab.setCustomView(adapter.getTabView(i));
+        }
+        TabLayout.Tab tab = tabLayout.getTabAt(position);
+        assert tab != null;
+        tab.setCustomView(null);
+        tab.setCustomView(adapter.getSelectedTabView(position));
+    }
+}
