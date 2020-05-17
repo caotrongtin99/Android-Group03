c484b2daf509aeaa3f239e4d4fcc2ed5a7f854a0
ltthinh1999 <thinhle111199@gmail.com>
2020-05-17 13:25:37 +0700

    artists

--- MusicApp/app/src/main/java/com/example/musicapp/artists/ArtistFragment.java
+++ MusicApp/app/src/main/java/com/example/musicapp/artists/ArtistFragment.java
@@ -1,4 +1,4 @@
-package com.example.musicapp;
+package com.example.musicapp.artists;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.musicapp.R;
 
 public class ArtistFragment extends Fragment {
     @Override
