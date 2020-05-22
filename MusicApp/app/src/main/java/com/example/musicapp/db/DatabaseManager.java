70a732c56cbc982ca4f10e59e1a510498f401617
Cao Trong Tin <35460708+caotrongtin99@users.noreply.github.com>
2020-05-22 15:41:47 +0700

    update listview

---
+++ MusicApp/app/src/main/java/com/example/musicapp/db/DatabaseManager.java
@@ -0,0 +1,78 @@
+package com.example.musicapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.musicapp.tracks.Track;
+
+public class DatabaseManager extends SQLiteOpenHelper {
+
+    private static DatabaseManager mDatabaseInstance = null;
+
+    private SQLiteDatabase mDatabase;
+    private Context mContext;
+    private int mOpenCounter;
+
+    public static DatabaseManager getInstance() {
+        return mDatabaseInstance;
+    }
+
+    // Database Version
+    private static final int DATABASE_VERSION = 11;
+
+    // Database Name
+    public static final String DATABASE_NAME = "music_app.db";
+    //singleton
+    public static DatabaseManager newInstance(Context context) {
+        if (mDatabaseInstance == null) {
+            mDatabaseInstance = new DatabaseManager(context.getApplicationContext());
+        }
+        return mDatabaseInstance;
+    }
+
+    public DatabaseManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
+
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(Track.SCRIPT_CREATE_TABLE);
+        /*
+        db.execSQL(PlayModel.SCRIPT_CREATE_TABLE);
+        db.execSQL(PlaylistModel.SCRIPT_CREATE_TABLE);
+        db.execSQL(PlaylistSongModel.SCRIPT_CREATE_TABLE);
+        db.execSQL(RecentModel.SCRIPT_CREATE_TABLE); */
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + Track.TABLE_NAME);
+        /*
+        db.execSQL("DROP TABLE IF EXISTS " + PlayModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PlaylistModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PlaylistSongModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + RecentModel.TABLE_NAME);
+        onCreate(db);
+
+         */
+    }
+
+    public void resetDB() {
+        /*
+        SQLiteDatabase db = getReadableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + SongModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PlayModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PlaylistModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PlaylistSongModel.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + RecentModel.TABLE_NAME);
+        onCreate(db);
+
+         */
+    }
+}
